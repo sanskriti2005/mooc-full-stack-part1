eea3f60568ffddfb491a5f5a2da4b892b2a545e1
Sanskriti <sanskritisri27@gmail.com>
2025-04-17 17:14:54 +0530

    part 1.9

--- unicafe-part1.1-part/src/App.jsx
+++ unicafe-part1.1-part/src/App.jsx
@@ -9,17 +9,24 @@ const Statisitcs = ({ good, neutral, bad }) => {
   };
 
   const positive = total == 0 ? 0 : (good / total) * 100;
+  if (total != 0) {
+    return (
+      <>
+        <h2>statistics</h2>
+        <p>good {good}</p>
+        <p>neutral {neutral}</p>
+        <p>bad {bad}</p>
+        <p>all {total}</p>
+        <p>average {avg()}</p>
+        <p>positive {positive} %</p>
+      </>
+    );
+  } 
   return (
     <>
-      <h2>statistics</h2>
-      <p>good {good}</p>
-      <p>neutral {neutral}</p>
-      <p>bad {bad}</p>
-      <p>all {total}</p>
-      <p>average {avg()}</p>
-      <p>positive {positive} %</p>
+    <p>No feedback given</p>
     </>
-  );
+  )
 };
 
 function App() {
