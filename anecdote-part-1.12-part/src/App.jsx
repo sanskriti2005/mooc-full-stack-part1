2b03bb6d5199a222dd8a811fc17f902820fe503f
Sanskriti <sanskritisri27@gmail.com>
2025-04-22 10:19:18 +0530

    anecdote setup and 1.12

---
+++ anecdote-part-1.12-part/src/App.jsx
@@ -0,0 +1,41 @@
+import { useState } from "react";
+
+const App = () => {
+  const anecdotes = [
+    ["If it hurts, do it more often.", 0],
+    ["Adding manpower to a late software project makes it later!", 0],
+    [
+      "The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.",
+      0,
+    ],
+    [
+      "Any fool can write code that a computer can understand. Good programmers write code that humans can understand.",
+      0,
+    ],
+    ["Premature optimization is the root of all evil.", 0],
+    [
+      "Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.",
+      0,
+    ],
+    [
+      "Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.",
+      0,
+    ],
+    ["The only way to go fast, is to go well.", 0],
+  ];
+
+  const [selected, setSelected] = useState(0);
+
+  const nextAnecdote = () => {
+    let randomNum = Math.floor(Math.random() * anecdotes.length + 1);
+    setSelected(randomNum);
+  };
+  return (
+    <>
+      <p>{anecdotes[selected][0]}</p>
+      <button onClick={() => nextAnecdote()}>next anecdote</button>
+    </>
+  );
+};
+
+export default App;
